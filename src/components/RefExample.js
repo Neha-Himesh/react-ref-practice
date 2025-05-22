254a3f23a9462096973a0f65ba1eb3f70c1420cf
Neha-Himesh <neha.himesh@spike.sh>
2025-05-22 12:30:10 +0530

    Added createRef() to focus input element using react

--- src/components/RefExample.js
+++ src/components/RefExample.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 
-export class RefExample extends Component {
+class RefExample extends Component {
     constructor(props) {
       super(props)
     
         this.inputRef = React.createRef()
+        this.cbRef = null
+        this.setCbRef = (element) =>{
+            this.cbRef = element
+        }
        
     }
     componentDidMount(){
-        this.inputRef.current.focus()
+        // this.inputRef.current.focus()
+        if(this.cbRef){
+            this.cbRef.focus()
+        }
     }
     focusInput =()=>{
         alert(this.inputRef.current.value)
@@ -17,6 +24,7 @@ export class RefExample extends Component {
     return (
         <div>
             <input type="text" ref={this.inputRef}/>
+            <input type="text" ref={this.setCbRef}/>
             <button onClick={this.focusInput}>Click Me</button>
         </div>
     )
